6eb320590976239884b0a7571920d8236a144d38
Phillip Wong <deepelegant99@gmail.com>
2021-10-16 23:42:41 -0700

    Created starter components, pages, and setup react rounters, css files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
-import './App.css';
+import { BrowserRouter, Switch, Route} from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path='/' exact>
+            <HomePage />
+          </Route>
+          <Route path='/profiles'>
+            <ProfilePage />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
