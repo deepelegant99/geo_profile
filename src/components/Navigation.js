e5aa654111d947fee989b9643990cf6f1f1941ee
Henry Crossley <crossh2001@yahoo.com>
2021-10-20 23:35:31 -0700

    added button to links and card

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,29 @@
 import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
+import { StylesProvider } from "@material-ui/core/styles";
+import "../App.css";
+
 
 const Navigation = () => {
   return (
+    <StylesProvider>
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" style={{ textDecoration: 'none'}}>
+            <Button style={{backgroundColor: 'gray', color:'white',
+              fontFamily: 'Arial, Helvetica, sans-serif',
+              fontSize: '1.5rem', width:'150px', borderRadius: '50px'}}>Home</Button></Link>
         </li>
         <li>
-          <Link to="/profiles">Profiles</Link>
+          <Link to="/profiles" style={{ textDecoration: 'none' }}>
+            <Button style={{backgroundColor: 'gray', color:'white',
+          fontFamily: 'Arial, Helvetica, sans-serif',
+          fontSize: '1.5rem', borderRadius: '50px'}}>Profiles</Button></Link>
         </li>
       </ul>
     </nav>
+    </StylesProvider>
   );
 };
 
